19233a3828246cb8d9ae19799876befbe7b1e8ee
matsuda akihito <matsuda104@gmail.com>
2015-10-18 17:27:20 +0900

    initial commit

---
+++ app/src/main/java/com/freshdigitable/udonroad/MainActivity.java
@@ -0,0 +1,11 @@
+package com.freshdigitable.udonroad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity
+public class MainActivity extends AppCompatActivity {
+
+}
