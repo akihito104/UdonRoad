9c6ab218370f11b5c886741c2356e20c6008740d
matsuda akihito <matsuda104@gmail.com>
2015-10-18 19:55:51 +0900

    added recycler view for timeline

--- app/src/main/java/com/freshdigitable/udonroad/MainActivity.java
+++ app/src/main/java/com/freshdigitable/udonroad/MainActivity.java
@@ -2,10 +2,29 @@ package com.freshdigitable.udonroad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
-@EActivity
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
+  @ViewById(R.id.timeline)
+  RecyclerView timeline;
+
+  private RecyclerView.Adapter tlAdapter;
+  private RecyclerView.LayoutManager tlLayoutManager;
+
+  @AfterViews
+  void afterViews() {
+    timeline.setHasFixedSize(true);
+    tlLayoutManager = new LinearLayoutManager(this);
+    timeline.setLayoutManager(tlLayoutManager);
+    tlAdapter = new TimelineAdapter();
+    timeline.setAdapter(tlAdapter);
+  }
 
 }
+
