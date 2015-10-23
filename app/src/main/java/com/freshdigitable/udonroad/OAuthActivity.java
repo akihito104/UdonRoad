af7bb22df232b5463e4ed6b8ef6d98583452f214
matsuda akihito <matsuda104@gmail.com>
2015-10-23 23:32:24 +0900

    wip) added oauth authorization

--- app/src/main/java/com/freshdigitable/udonroad/OAuthActivity.java
+++ app/src/main/java/com/freshdigitable/udonroad/OAuthActivity.java
@@ -1,25 +1,35 @@
 package com.freshdigitable.udonroad;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.Toast;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.ViewById;
 import org.androidannotations.annotations.res.StringRes;
 
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.auth.RequestToken;
 
 /**
  * Created by akihit on 15/10/22.
  */
 @EActivity(R.layout.activity_login)
 public class OAuthActivity extends AppCompatActivity {
+  private static final String TAG = OAuthActivity.class.getName();
+
   @StringRes(R.string.callback_url)
   String callbackUrl;
 
-  @ViewById
+  @ViewById(R.id.button_oauth)
   Button oauthButton;
 
   private Twitter twitter;
@@ -29,8 +39,48 @@ public class OAuthActivity extends AppCompatActivity {
     twitter = AccessUtil.getTwitterInstance(this);
   }
 
-  @Click
+  @Click(R.id.button_oauth)
   protected void onClicked() {
+    startAuthorization();
+  }
+
+  private RequestToken requestToken;
+
+  @Background
+  protected void startAuthorization() {
+    try {
+      requestToken = twitter.getOAuthRequestToken(callbackUrl);
+    } catch (TwitterException e) {
+      Log.e(TAG, "Authorization error: ", e);
+      return;
+    }
+    String authUrl = requestToken.getAuthorizationURL();
+    startAuthAction(authUrl);
+  }
+
+  @UiThread
+  protected void startAuthAction(String url) {
+    if (url == null) {
+      Toast.makeText(this, "authorization is failed...", Toast.LENGTH_LONG).show();
+    }
+    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+    startActivity(intent);
+  }
+
+  @Override
+  protected void onNewIntent(Intent intent) {
+    if (intent == null) {
+      return;
+    }
+    Uri uri = intent.getData();
+    if (uri == null) {
+      return;
+    }
+    if (!uri.toString().startsWith(callbackUrl)) {
+      return;
+    }
+
+    //todo: start authentication
   }
 }
 
