43dbd7841b11714c55f9a2ed133f2be95262d311
matsuda akihito <matsuda104@gmail.com>
2015-10-25 20:48:18 +0900

    wip) added to load home timeline once

--- app/src/main/java/com/freshdigitable/udonroad/MainActivity.java
+++ app/src/main/java/com/freshdigitable/udonroad/MainActivity.java
@@ -1,22 +1,57 @@
 package com.freshdigitable.udonroad;
 
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.ViewById;
+
+import java.util.List;
+
+import twitter4j.ResponseList;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
-  @ViewById(R.id.timeline)
-  RecyclerView timeline;
+  private static final String TAG = MainActivity.class.getName();
 
-  private RecyclerView.Adapter tlAdapter;
+  @ViewById(R.id.timeline)
+  protected RecyclerView timeline;
+
+  private TimelineAdapter tlAdapter;
   private RecyclerView.LayoutManager tlLayoutManager;
+  private final RecyclerView.ItemDecoration itemDecoration = new MyItemDecoration();
+
+  private static class MyItemDecoration extends RecyclerView.ItemDecoration {
+    MyItemDecoration() {
+      paint.setColor(Color.GRAY);
+    }
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+      outRect.set(0, 0, 0, 2);
+    }
+
+    private final Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    @Override
+    public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+      super.onDraw(c, parent, state);
+      c.drawRect(0, 0, 0, 2, paint);
+    }
+  }
 
   @AfterViews
   void afterViews() {
@@ -27,11 +62,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     timeline.setHasFixedSize(true);
+    timeline.addItemDecoration(itemDecoration);
     tlLayoutManager = new LinearLayoutManager(this);
     timeline.setLayoutManager(tlLayoutManager);
-    tlAdapter = new TimelineAdapter();
-    timeline.setAdapter(tlAdapter);
+
+    fetchTweet();
   }
 
+  @Background
+  protected void fetchTweet() {
+    Twitter twitter = AccessUtil.getTwitterInstance(this);
+    try {
+      ResponseList<Status> statuses = twitter.getHomeTimeline();
+      updateTimeline(statuses);
+    } catch (TwitterException e) {
+      Log.e(TAG, "home timeline is not downloaded.", e);
+    }
+  }
+
+  @UiThread
+  protected void updateTimeline(List<Status> statuses) {
+    tlAdapter = new TimelineAdapter(statuses);
+    timeline.setAdapter(tlAdapter);
+  }
 }
 
--- app/src/main/java/com/freshdigitable/udonroad/TimelineAdapter.java
+++ app/src/main/java/com/freshdigitable/udonroad/TimelineAdapter.java
@@ -1,32 +1,68 @@
 package com.freshdigitable.udonroad;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import twitter4j.Status;
+import twitter4j.User;
+import twitter4j.util.TimeSpanConverter;
 
 /**
  * Created by akihit on 15/10/18.
  */
 public class TimelineAdapter extends RecyclerView.Adapter<TimelineAdapter.ViewHolder> {
-  @Override
-  public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-    return null;
+  public TimelineAdapter(List<Status> statuses) {
+    this.statuses = statuses;
   }
 
   @Override
-  public void onBindViewHolder(ViewHolder holder, int position) {
+  public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    View view = LayoutInflater.from(parent.getContext())
+        .inflate(R.layout.tweet_view, parent, false);
+    return new ViewHolder(view);
+  }
 
+  private List<Status> statuses;
+  private static final TimeSpanConverter timeSpanConv = new TimeSpanConverter();
+
+  @Override
+  public void onBindViewHolder(ViewHolder holder, int position) {
+    Status status = statuses.get(position);
+    User user = status.getUser();
+    Picasso.with(holder.icon.getContext())
+        .load(user.getOriginalProfileImageURL()).into(holder.icon);
+    holder.account.setText(user.getName());
+    holder.screenName.setText(user.getScreenName());
+    holder.tweet.setText(status.getText());
+    holder.time.setText(timeSpanConv.toTimeSpanString(status.getCreatedAt()));
   }
 
   @Override
   public int getItemCount() {
-    return 0;
+    return this.statuses.size();
   }
 
   public static class ViewHolder extends RecyclerView.ViewHolder {
-
+    ImageView icon;
+    TextView screenName;
+    TextView account;
+    TextView tweet;
+    TextView time;
     public ViewHolder(View itemView) {
       super(itemView);
+      this.icon = (ImageView) itemView.findViewById(R.id.tl_icon);
+      this.screenName = (TextView) itemView.findViewById(R.id.tl_displayname);
+      this.account = (TextView) itemView.findViewById(R.id.tl_account);
+      this.tweet = (TextView) itemView.findViewById(R.id.tl_tweet);
+      this.time = (TextView) itemView.findViewById(R.id.tl_time);
     }
   }
 }
