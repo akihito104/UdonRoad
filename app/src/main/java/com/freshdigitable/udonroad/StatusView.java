cbe8cd0e9644bfd13b276c26313ee16d050fb24a
matsuda akihito <matsuda104@gmail.com>
2016-01-11 20:26:37 +0900

    refactored child view of recyclerview #10

---
+++ app/src/main/java/com/freshdigitable/udonroad/StatusView.java
@@ -0,0 +1,135 @@
+package com.freshdigitable.udonroad;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.text.Html;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import twitter4j.Status;
+import twitter4j.User;
+import twitter4j.util.TimeSpanConverter;
+
+/**
+ * Created by akihit on 2016/01/11.
+ */
+public class StatusView extends RelativeLayout {
+  private long incomingTweetId;
+  private ImageView icon;
+  private TextView screenName;
+  private TextView at;
+  private TextView account;
+  private TextView tweet;
+  private TextView time;
+
+  private TextView rt;
+  private TextView rtCount;
+  private TextView fav;
+  private TextView favCount;
+  private TextView clientName;
+
+  private TextView rtby;
+  private TextView retweetedUser;
+
+  public StatusView(Context context) {
+    this(context, null);
+  }
+
+  public StatusView(Context context, AttributeSet attrs) {
+    this(context, attrs, 0);
+  }
+
+  public StatusView(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    LayoutInflater.from(context).inflate(R.layout.tweet_view, this, true);
+//    inflate(context, R.layout.tweet_view, this);
+    this.icon = (ImageView) findViewById(R.id.tl_icon);
+    this.screenName = (TextView) findViewById(R.id.tl_displayname);
+    this.at = (TextView) findViewById(R.id.tl_at);
+    this.account = (TextView) findViewById(R.id.tl_account);
+    this.tweet = (TextView) findViewById(R.id.tl_tweet);
+    this.time = (TextView) findViewById(R.id.tl_time);
+
+    this.rt = (TextView) findViewById(R.id.tl_rt);
+    this.rtCount = (TextView) findViewById(R.id.tl_rtcount);
+    this.fav = (TextView) findViewById(R.id.tl_fav);
+    this.favCount = (TextView) findViewById(R.id.tl_favcount);
+    this.clientName = (TextView) findViewById(R.id.tl_clientname);
+
+    this.rtby = (TextView) findViewById(R.id.tl_rtby);
+    this.retweetedUser = (TextView) findViewById(R.id.tl_rtuser);
+  }
+
+  private static final int RETWEETED_TEXT_COLOR = Color.argb(255, 64, 192, 64);
+  private static final TimeSpanConverter timeSpanConv = new TimeSpanConverter();
+
+  public void setStatus(final Status status) {
+    incomingTweetId = status.getId();
+    if (status.isRetweet()) {
+      setRetweetedUserVisibility(View.VISIBLE);
+      this.retweetedUser.setText(status.getUser().getScreenName());
+      this.setTextColor(RETWEETED_TEXT_COLOR);
+    }
+    Status bindingStatus;
+    if (status.isRetweet()) {
+      bindingStatus = status.getRetweetedStatus();
+    } else {
+      bindingStatus = status;
+    }
+    User user = bindingStatus.getUser();
+    Picasso.with(this.icon.getContext())
+        .load(user.getProfileImageURLHttps()).fit().into(this.icon);
+
+    this.account.setText(user.getName());
+    this.screenName.setText(user.getScreenName());
+    this.tweet.setText(bindingStatus.getText());
+    this.time.setText(timeSpanConv.toTimeSpanString(bindingStatus.getCreatedAt()));
+    this.clientName.setText(Html.fromHtml(bindingStatus.getSource()).toString());
+
+    final int rtCount = bindingStatus.getRetweetCount();
+    if (rtCount > 0) {
+      this.setRtCountVisibility(View.VISIBLE);
+      this.rtCount.setText(String.valueOf(rtCount));
+    }
+
+    final int favCount = bindingStatus.getFavoriteCount();
+    if (favCount > 0) {
+      this.setFavCountVisibility(View.VISIBLE);
+      this.favCount.setText(String.valueOf(favCount));
+    }
+  }
+
+  public long getIncomingTweetId() {
+    return this.incomingTweetId;
+  }
+
+  public void setRtCountVisibility(int visibility) {
+    this.rt.setVisibility(visibility);
+    this.rtCount.setVisibility(visibility);
+  }
+
+  public void setFavCountVisibility(int visibility) {
+    this.fav.setVisibility(visibility);
+    this.favCount.setVisibility(visibility);
+  }
+
+  public void setRetweetedUserVisibility(int visibility) {
+    this.rtby.setVisibility(visibility);
+    this.retweetedUser.setVisibility(visibility);
+  }
+
+  public void setTextColor(int color) {
+    this.screenName.setTextColor(color);
+    this.at.setTextColor(color);
+    this.account.setTextColor(color);
+    this.time.setTextColor(color);
+    this.tweet.setTextColor(color);
+  }
+}
--- app/src/main/java/com/freshdigitable/udonroad/TimelineAdapter.java
+++ app/src/main/java/com/freshdigitable/udonroad/TimelineAdapter.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.support.annotation.MainThread;
 import android.support.v7.widget.RecyclerView;
 import android.text.Html;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,9 +36,7 @@ public class TimelineAdapter extends RecyclerView.Adapter<TimelineAdapter.ViewHo
 
   @Override
   public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-    View view = LayoutInflater.from(parent.getContext())
-        .inflate(R.layout.tweet_view, parent, false);
-    return new ViewHolder(view);
+    return new ViewHolder(new StatusView(parent.getContext()));
   }
 
   private final List<Status> statuses;
@@ -52,27 +51,21 @@ public class TimelineAdapter extends RecyclerView.Adapter<TimelineAdapter.ViewHo
   }
 
   private View selectedView = null;
-  private static final TimeSpanConverter timeSpanConv = new TimeSpanConverter();
 
   @Override
   public void onBindViewHolder(final ViewHolder holder, int position) {
     Status status = statuses.get(position);
-    final long incomingTweetId = status.getId();
-    if (status.isRetweet()) {
-      holder.setRetweetedUserVisibility(View.VISIBLE);
-      holder.retweetedUser.setText(status.getUser().getScreenName());
-      status = status.getRetweetedStatus();
-      holder.setTextColor(Color.argb(255, 64, 192, 64));
-    }
-    User user = status.getUser();
-    Picasso.with(holder.icon.getContext())
-        .load(user.getProfileImageURLHttps()).fit().into(holder.icon);
+    holder.statusView.setStatus(status);
 
-    holder.view.setOnClickListener(new View.OnClickListener() {
+    holder.statusView.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        if (selectedTweetId != incomingTweetId) {
-          selectedTweetId = incomingTweetId;
+        if (!(view instanceof StatusView)) {
+          return;
+        }
+        StatusView sv = (StatusView) view;
+        if (selectedTweetId != sv.getIncomingTweetId()) {
+          selectedTweetId = sv.getIncomingTweetId();
           if (selectedView != null) {
             selectedView.setBackgroundColor(Color.TRANSPARENT);
           }
@@ -83,37 +76,19 @@ public class TimelineAdapter extends RecyclerView.Adapter<TimelineAdapter.ViewHo
         }
       }
     });
-    holder.view.setBackgroundColor(Color.TRANSPARENT);
     if (status.getId() == selectedTweetId) {
-      holder.view.setBackgroundColor(Color.LTGRAY);
-      selectedView = holder.view;
-    }
-    holder.tweetId = status.getId();
-    holder.account.setText(user.getName());
-    holder.screenName.setText(user.getScreenName());
-    holder.tweet.setText(status.getText());
-    holder.time.setText(timeSpanConv.toTimeSpanString(status.getCreatedAt()));
-    holder.clientName.setText(Html.fromHtml(status.getSource()).toString());
-
-    final int rtCount = status.getRetweetCount();
-    if (rtCount > 0) {
-      holder.setRtCountVisibility(View.VISIBLE);
-      holder.rtCount.setText(String.valueOf(rtCount));
-    }
-
-    final int favCount = status.getFavoriteCount();
-    if (favCount > 0) {
-      holder.setFavCountVisibility(View.VISIBLE);
-      holder.favCount.setText(String.valueOf(favCount));
+      holder.statusView.setBackgroundColor(Color.LTGRAY);
+      selectedView = holder.statusView;
     }
   }
 
   @Override
   public void onViewRecycled(ViewHolder holder) {
-    holder.setRtCountVisibility(View.GONE);
-    holder.setFavCountVisibility(View.GONE);
-    holder.setRetweetedUserVisibility(View.GONE);
-    holder.setTextColor(Color.GRAY);
+    holder.statusView.setBackgroundColor(Color.TRANSPARENT);
+    holder.statusView.setRtCountVisibility(View.GONE);
+    holder.statusView.setFavCountVisibility(View.GONE);
+    holder.statusView.setRetweetedUserVisibility(View.GONE);
+    holder.statusView.setTextColor(Color.GRAY);
   }
 
   @Override
@@ -162,65 +137,11 @@ public class TimelineAdapter extends RecyclerView.Adapter<TimelineAdapter.ViewHo
   }
 
   public static class ViewHolder extends RecyclerView.ViewHolder {
-    long tweetId;
-    View view;
-    ImageView icon;
-    TextView screenName;
-    TextView at;
-    TextView account;
-    TextView tweet;
-    TextView time;
-
-    TextView rt;
-    TextView rtCount;
-    TextView fav;
-    TextView favCount;
-    TextView clientName;
-
-    TextView rtby;
-    TextView retweetedUser;
+    private StatusView statusView;
 
     public ViewHolder(View itemView) {
       super(itemView);
-      this.view = itemView;
-      this.icon = (ImageView) itemView.findViewById(R.id.tl_icon);
-      this.screenName = (TextView) itemView.findViewById(R.id.tl_displayname);
-      this.at = (TextView) itemView.findViewById(R.id.tl_at);
-      this.account = (TextView) itemView.findViewById(R.id.tl_account);
-      this.tweet = (TextView) itemView.findViewById(R.id.tl_tweet);
-      this.time = (TextView) itemView.findViewById(R.id.tl_time);
-
-      this.rt = (TextView) itemView.findViewById(R.id.tl_rt);
-      this.rtCount = (TextView) itemView.findViewById(R.id.tl_rtcount);
-      this.fav = (TextView) itemView.findViewById(R.id.tl_fav);
-      this.favCount = (TextView) itemView.findViewById(R.id.tl_favcount);
-      this.clientName = (TextView) itemView.findViewById(R.id.tl_clientname);
-
-      this.rtby = (TextView) itemView.findViewById(R.id.tl_rtby);
-      this.retweetedUser = (TextView) itemView.findViewById(R.id.tl_rtuser);
-    }
-
-    public void setRtCountVisibility(int visibility) {
-      this.rt.setVisibility(visibility);
-      this.rtCount.setVisibility(visibility);
-    }
-
-    public void setFavCountVisibility(int visibility) {
-      this.fav.setVisibility(visibility);
-      this.favCount.setVisibility(visibility);
-    }
-
-    public void setRetweetedUserVisibility(int visibility) {
-      this.rtby.setVisibility(visibility);
-      this.retweetedUser.setVisibility(visibility);
-    }
-
-    public void setTextColor(int color) {
-      this.screenName.setTextColor(color);
-      this.at.setTextColor(color);
-      this.account.setTextColor(color);
-      this.time.setTextColor(color);
-      this.tweet.setTextColor(color);
+      this.statusView = (StatusView) itemView;
     }
   }
 }
