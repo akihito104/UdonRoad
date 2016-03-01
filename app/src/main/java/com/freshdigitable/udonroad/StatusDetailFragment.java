a81e289a63fd04ea8ded93b6e9cf62c974cce0e9
matsuda akihito <matsuda104@gmail.com>
2016-03-01 23:51:45 +0900

    wip) added detail menu for each status

--- app/src/main/java/com/freshdigitable/udonroad/StatusDetailFragment.java
+++ app/src/main/java/com/freshdigitable/udonroad/StatusDetailFragment.java
@@ -7,10 +7,13 @@ package com.freshdigitable.udonroad;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.freshdigitable.udonroad.databinding.FragmentStatusDetailBinding;
 
@@ -23,16 +26,34 @@ public class StatusDetailFragment extends Fragment {
   @Nullable
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    View view = inflater.inflate(R.layout.fragment_status_detail, container, false);
-    binding = DataBindingUtil.bind(view);
-    return view;
+    return  inflater.inflate(R.layout.fragment_status_detail, container, false);
   }
 
   @Override
   public void onActivityCreated(@Nullable Bundle savedInstanceState) {
     super.onActivityCreated(savedInstanceState);
+    binding = DataBindingUtil.bind(getView());
+
     this.status = (Status) getArguments().get("status");
     binding.statusView.bindStatus(this.status);
+
+    final ArrayAdapter<DetailMenu> arrayAdapter
+        = new ArrayAdapter<DetailMenu>(getContext(), android.R.layout.simple_list_item_1){
+      @Override
+      public View getView(int position, View convertView, ViewGroup parent) {
+        View v = super.getView(position, convertView, parent);
+        final DetailMenu item = getItem(position);
+        final int strres = item.getStringRes();
+        ((TextView)v).setText(strres);
+        return v;
+      }
+    };
+
+    binding.detailMenu.setAdapter(arrayAdapter);
+    arrayAdapter.add(status.isRetweetedByMe() ? DetailMenu.RT_DELETE : DetailMenu.RT_CREATE);
+    arrayAdapter.add(status.isFavorited() ? DetailMenu.FAV_DELETE : DetailMenu.FAV_CREATE);
+    arrayAdapter.add(DetailMenu.REPLY);
+    arrayAdapter.add(DetailMenu.QUOTE);
   }
 
   public static StatusDetailFragment getInstance(final Status status) {
@@ -41,5 +62,44 @@ public class StatusDetailFragment extends Fragment {
     final StatusDetailFragment statusDetailFragment = new StatusDetailFragment();
     statusDetailFragment.setArguments(args);
     return statusDetailFragment;
+  }
+
+  enum DetailMenu implements DetailMenuInterface {
+    RT_CREATE {
+      @Override
+      public int getStringRes() {
+        return R.string.detail_rt_create;
+      }
+    }, RT_DELETE {
+      @Override
+      public int getStringRes() {
+        return R.string.detail_rt_delete;
+      }
+    }, FAV_CREATE {
+      @Override
+      public int getStringRes() {
+        return R.string.detail_fav_create;
+      }
+    }, FAV_DELETE {
+      @Override
+      public int getStringRes() {
+        return R.string.detail_fav_delete;
+      }
+    }, REPLY {
+      @Override
+      public int getStringRes() {
+        return R.string.detail_reply;
+      }
+    }, QUOTE {
+      @Override
+      public int getStringRes() {
+        return R.string.detail_quote;
+      }
+    },;
+  }
+
+  interface DetailMenuInterface {
+    @StringRes
+    int getStringRes();
   }
 }
