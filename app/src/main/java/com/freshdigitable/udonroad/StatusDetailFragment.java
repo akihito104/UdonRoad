a6ad80126b5c208308b3eaf947546741ee62d881
matsuda akihito <matsuda104@gmail.com>
2016-03-02 23:59:58 +0900

    wip) added detail menu for each status

--- app/src/main/java/com/freshdigitable/udonroad/StatusDetailFragment.java
+++ app/src/main/java/com/freshdigitable/udonroad/StatusDetailFragment.java
@@ -12,11 +12,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.freshdigitable.udonroad.databinding.FragmentStatusDetailBinding;
+import com.google.repacked.kotlin.jvm.functions.Function1;
 
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action0;
+import rx.functions.Action1;
 import twitter4j.Status;
 
 public class StatusDetailFragment extends Fragment {
@@ -54,6 +61,19 @@ public class StatusDetailFragment extends Fragment {
     arrayAdapter.add(status.isFavorited() ? DetailMenu.FAV_DELETE : DetailMenu.FAV_CREATE);
     arrayAdapter.add(DetailMenu.REPLY);
     arrayAdapter.add(DetailMenu.QUOTE);
+    binding.detailMenu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+      @Override
+      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        final DetailMenu menu = (DetailMenu) parent.getItemAtPosition(position);
+        menu.action(twitterApi, status);
+      }
+    });
+  }
+
+  private TwitterApi twitterApi;
+
+  public void setTwitterApi(TwitterApi twitterApi) {
+    this.twitterApi = twitterApi;
   }
 
   public static StatusDetailFragment getInstance(final Status status) {
@@ -69,6 +89,27 @@ public class StatusDetailFragment extends Fragment {
       @Override
       public int getStringRes() {
         return R.string.detail_rt_create;
+      }
+
+      @Override
+      public void action(TwitterApi api, Status status) {
+        api.retweetStatus(status.getId())
+            .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(new Action1<Status>() {
+          @Override
+          public void call(Status status) {
+
+          }
+        }, new Action1<Throwable>() {
+          @Override
+          public void call(Throwable throwable) {
+
+          }
+        }, new Action0() {
+          @Override
+          public void call() {
+          }
+        });
       }
     }, RT_DELETE {
       @Override
@@ -101,5 +142,19 @@ public class StatusDetailFragment extends Fragment {
   interface DetailMenuInterface {
     @StringRes
     int getStringRes();
+
+    void action(TwitterApi api, Status status);
+  }
+
+  final Function1<String, Action0> toastAction = new Function1<String, Action0>() {
+    @Override
+    public Action0 invoke(String s) {
+      return new Action1<String>() {
+        @Override
+        public void call(String s) {
+
+        }
+      };
+    }
   }
 }
