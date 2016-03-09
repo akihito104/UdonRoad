993a8831ee343fbbcea97e50598b7b39748c586f
matsuda akihito <matsuda104@gmail.com>
2016-03-09 23:44:33 +0900

    refactored MainActivity and TimelineFragment

--- app/src/main/java/com/freshdigitable/udonroad/MainActivity.java
+++ app/src/main/java/com/freshdigitable/udonroad/MainActivity.java
@@ -24,20 +24,11 @@ import android.widget.Toast;
 import com.freshdigitable.udonroad.databinding.ActivityMainBinding;
 import com.squareup.picasso.Picasso;
 
-import java.util.List;
-
 import rx.Observable;
 import rx.Observer;
-import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action1;
-import twitter4j.Paging;
-import twitter4j.StallWarning;
 import twitter4j.Status;
-import twitter4j.StatusDeletionNotice;
 import twitter4j.User;
-import twitter4j.UserStreamAdapter;
-import twitter4j.UserStreamListener;
 
 public class MainActivity extends AppCompatActivity {
   private static final String TAG = MainActivity.class.getSimpleName();
@@ -80,7 +71,6 @@ public class MainActivity extends AppCompatActivity {
 
     setupAppBar();
     setupUserTimeline();
-    fetchTweet();
   }
 
   private void setupAppBar() {
@@ -95,24 +85,7 @@ public class MainActivity extends AppCompatActivity {
 
   private void setupUserTimeline() {
     tlFragment = new TimelineFragment();
-    tlFragment.setLastItemBoundListener(new TimelineAdapter.LastItemBoundListener() {
-      @Override
-      public void onLastItemBound(long statusId) {
-        fetchTweet(new Paging(1, 20, 1, statusId - 1));
-      }
-    });
-    tlFragment.setOnFlingForSelectedStatusListener(new TimelineFragment.OnFlingForSelectedStatusListener() {
-      @Override
-      public void onFling(FlingableFloatingActionButton.Direction direction, Status status) {
-        if (FlingableFloatingActionButton.Direction.UP.equals(direction)) {
-          fetchFavorite(status.getId());
-        } else if (FlingableFloatingActionButton.Direction.RIGHT.equals(direction)) {
-          fetchRetweet(status.getId());
-        } else if (FlingableFloatingActionButton.Direction.LEFT.equals(direction)) {
-          showStatusDetail(status);
-        }
-      }
-    });
+    tlFragment.setTwitterApi(twitterApi);
     tlFragment.setUserIconClickedListener(new TimelineAdapter.OnUserIconClickedListener() {
       @Override
       public void onClicked(User user) {
@@ -125,25 +98,6 @@ public class MainActivity extends AppCompatActivity {
         .commit();
   }
 
-  private StatusDetailFragment statusDetail;
-
-  private void showStatusDetail(Status status) {
-    statusDetail = StatusDetailFragment.getInstance(status);
-    statusDetail.setTwitterApi(twitterApi);
-    getSupportFragmentManager().beginTransaction()
-        .hide(tlFragment)
-        .add(R.id.main_timeline, statusDetail)
-        .commit();
-    tlFragment.setStopScroll(true);
-  }
-
-  private void hideStatusDetail() {
-    getSupportFragmentManager().beginTransaction()
-        .remove(statusDetail)
-        .show(tlFragment)
-        .commit();
-    tlFragment.setStopScroll(false);
-  }
 
   private void showUserInfo(User user) {
     final UserInfoHeaderFragment userInfoHeader = UserInfoHeaderFragment.getInstance(user);
@@ -215,19 +169,16 @@ public class MainActivity extends AppCompatActivity {
   @Override
   protected void onResume() {
     super.onResume();
-    twitterApi.connectUserStream(statusListener);
   }
 
   @Override
   protected void onPause() {
-    twitterApi.disconnectStreamListener();
     super.onPause();
   }
 
   @Override
   public void onBackPressed() {
-    if (statusDetail != null && statusDetail.isVisible()) {
-      hideStatusDetail();
+    if (tlFragment.hideStatusDetail()) {
       return;
     }
     if (binding.navDrawerLayout.isDrawerOpen(binding.navDrawer)) {
@@ -307,134 +258,6 @@ public class MainActivity extends AppCompatActivity {
     sendStatusMenuItem.setIcon(R.drawable.ic_create_white_24dp);
     tlFragment.setStopScroll(false);
     appbarFragment.collapseStatusInputView();
-  }
-
-  private final UserStreamListener statusListener = new UserStreamAdapter() {
-    @Override
-    public void onStatus(final Status status) {
-      Observable.just(status)
-          .observeOn(AndroidSchedulers.mainThread())
-          .subscribe(new Action1<Status>() {
-            @Override
-            public void call(Status status) {
-              tlFragment.addNewStatus(status);
-            }
-          });
-    }
-
-    @Override
-    public void onDeletionNotice(final StatusDeletionNotice statusDeletionNotice) {
-      Log.d(TAG, statusDeletionNotice.toString());
-      Observable.just(statusDeletionNotice.getStatusId())
-          .observeOn(AndroidSchedulers.mainThread())
-          .subscribe(new Action1<Long>() {
-            @Override
-            public void call(Long deletedStatusId) {
-              tlFragment.deleteStatus(deletedStatusId);
-            }
-          });
-    }
-
-    @Override
-    public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-      Log.d(TAG, "onTrackLimitationNotice: " + numberOfLimitedStatuses);
-    }
-
-    @Override
-    public void onScrubGeo(long userId, long upToStatusId) {
-      Log.d(TAG, "onScrubGeo: " + userId + ", " + upToStatusId);
-    }
-
-    @Override
-    public void onStallWarning(StallWarning warning) {
-      Log.d(TAG, "onStallWarning: " + warning.toString());
-    }
-
-    @Override
-    public void onException(Exception ex) {
-      Log.d(TAG, "onException: " + ex.toString());
-    }
-  };
-
-  private void fetchRetweet(final long tweetId) {
-    twitterApi.retweetStatus(tweetId)
-        .observeOn(AndroidSchedulers.mainThread())
-        .subscribe(new Subscriber<Status>() {
-          @Override
-          public void onCompleted() {
-            showToast("success to retweet");
-          }
-
-          @Override
-          public void onError(Throwable e) {
-            showToast("failed to retweet...");
-          }
-
-          @Override
-          public void onNext(Status status) {
-          }
-        });
-  }
-
-  private void fetchFavorite(final long tweetId) {
-    twitterApi.createFavorite(tweetId)
-        .observeOn(AndroidSchedulers.mainThread())
-        .subscribe(new Subscriber<Status>() {
-          @Override
-          public void onCompleted() {
-            showToast("success to create fav.");
-          }
-
-          @Override
-          public void onError(Throwable e) {
-            Log.e(TAG, "error: ", e);
-            showToast("failed to create fav...");
-          }
-
-          @Override
-          public void onNext(Status user) {
-          }
-        });
-  }
-
-  private void fetchTweet() {
-    twitterApi.getHomeTimeline()
-        .observeOn(AndroidSchedulers.mainThread())
-        .subscribe(new Subscriber<List<Status>>() {
-          @Override
-          public void onCompleted() {
-          }
-
-          @Override
-          public void onError(Throwable e) {
-            Log.e(TAG, "home timeline is not downloaded.", e);
-          }
-
-          @Override
-          public void onNext(List<Status> status) {
-            tlFragment.addNewStatuses(status);
-          }
-        });
-  }
-
-  private void fetchTweet(Paging paging) {
-    twitterApi.getHomeTimeline(paging)
-        .observeOn(AndroidSchedulers.mainThread())
-        .subscribe(new Subscriber<List<Status>>() {
-          @Override
-          public void onCompleted() {
-          }
-
-          @Override
-          public void onError(Throwable e) {
-            Log.e(TAG, "home timeline is not downloaded.", e);
-          }
-
-          @Override
-          public void onNext(List<Status> status) {
-            tlFragment.addStatusesAtLast(status);
-          }
-        });
   }
 
   private void showToast(String text) {
--- app/src/main/java/com/freshdigitable/udonroad/TimelineFragment.java
+++ app/src/main/java/com/freshdigitable/udonroad/TimelineFragment.java
@@ -9,15 +9,26 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.freshdigitable.udonroad.databinding.FragmentTimelineBinding;
 
 import java.util.List;
 
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import twitter4j.Paging;
+import twitter4j.StallWarning;
 import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.UserStreamAdapter;
+import twitter4j.UserStreamListener;
 
 public class TimelineFragment extends Fragment {
   @SuppressWarnings("unused")
@@ -25,6 +36,7 @@ public class TimelineFragment extends Fragment {
   private FragmentTimelineBinding binding;
   private final TimelineAdapter tlAdapter = new TimelineAdapter();
   private LinearLayoutManager tlLayoutManager;
+  private TwitterApi twitterApi;
 
   @Nullable
   @Override
@@ -40,28 +52,27 @@ public class TimelineFragment extends Fragment {
     binding.timeline.setHasFixedSize(true);
     RecyclerView.ItemDecoration itemDecoration = new TimelineDecoration();
     binding.timeline.addItemDecoration(itemDecoration);
-    tlLayoutManager = new LinearLayoutManager(getActivity());
+    tlLayoutManager = new LinearLayoutManager(getContext());
     binding.timeline.setLayoutManager(tlLayoutManager);
     binding.timeline.setItemAnimator(new TimelineAnimator());
     tlAdapter.setOnSelectedTweetChangeListener(
         new TimelineAdapter.OnSelectedTweetChangeListener() {
           @Override
           public void onTweetSelected(Status status) {
-            if (selectedTweetChangeListener != null) {
-              selectedTweetChangeListener.onTweetSelected(status);
-            }
             binding.fab.setVisibility(View.VISIBLE);
           }
 
           @Override
           public void onTweetUnselected() {
-            if (selectedTweetChangeListener != null) {
-              selectedTweetChangeListener.onTweetUnselected();
-            }
             binding.fab.setVisibility(View.GONE);
           }
         });
-    tlAdapter.setLastItemBoundListener(lastItemBoundListener);
+    tlAdapter.setLastItemBoundListener(new TimelineAdapter.LastItemBoundListener() {
+      @Override
+      public void onLastItemBound(long statusId) {
+        fetchTweet(new Paging(1, 20, 1, statusId - 1));
+      }
+    });
     binding.timeline.setAdapter(tlAdapter);
 
     binding.fab.setVisibility(View.GONE);
@@ -73,18 +84,57 @@ public class TimelineFragment extends Fragment {
     binding.fab.setOnFlingListener(new FlingableFloatingActionButton.OnFlingListener() {
       @Override
       public void onFling(FlingableFloatingActionButton.Direction direction) {
-        if (flingListener == null || !isTweetSelected()) {
+        if (!isTweetSelected()) {
           return;
         }
-        flingListener.onFling(direction, tlAdapter.getSelectedStatus());
+        final Status status = tlAdapter.getSelectedStatus();
+        if (FlingableFloatingActionButton.Direction.UP.equals(direction)) {
+          fetchFavorite(status.getId());
+        } else if (FlingableFloatingActionButton.Direction.RIGHT.equals(direction)) {
+          fetchRetweet(status.getId());
+        } else if (FlingableFloatingActionButton.Direction.LEFT.equals(direction)) {
+          showStatusDetail(status);
+        }
       }
     });
+    fetchTweet();
   }
 
-  private TimelineAdapter.LastItemBoundListener lastItemBoundListener;
+  @Override
+  public void onResume() {
+    super.onResume();
+    twitterApi.connectUserStream(statusListener);
+  }
 
-  public void setLastItemBoundListener(TimelineAdapter.LastItemBoundListener lastItemBoundListener) {
-    this.lastItemBoundListener = lastItemBoundListener;
+  @Override
+  public void onPause() {
+    twitterApi.disconnectStreamListener();
+    super.onPause();
+  }
+
+  private StatusDetailFragment statusDetail;
+
+  private void showStatusDetail(Status status) {
+    statusDetail = StatusDetailFragment.getInstance(status);
+    statusDetail.setTwitterApi(twitterApi);
+    getActivity().getSupportFragmentManager().beginTransaction()
+        .hide(this)
+        .add(R.id.main_timeline, statusDetail)
+        .commit();
+    setStopScroll(true);
+  }
+
+  public boolean hideStatusDetail() {
+    if (statusDetail == null) {
+      return false;
+    }
+    getActivity().getSupportFragmentManager().beginTransaction()
+        .remove(statusDetail)
+        .show(this)
+        .commit();
+    setStopScroll(false);
+    statusDetail = null;
+    return true;
   }
 
   private boolean stopScroll = false;
@@ -135,23 +185,143 @@ public class TimelineFragment extends Fragment {
     return tlAdapter.isStatusViewSelected();
   }
 
-  interface OnFlingForSelectedStatusListener {
-    void onFling(FlingableFloatingActionButton.Direction direction, Status status);
-  }
-
-  private OnFlingForSelectedStatusListener flingListener;
-
-  public void setOnFlingForSelectedStatusListener(OnFlingForSelectedStatusListener listener) {
-    this.flingListener = listener;
-  }
-
-  private TimelineAdapter.OnSelectedTweetChangeListener selectedTweetChangeListener;
-
-  public void setOnSelectedTweetChangeListener(TimelineAdapter.OnSelectedTweetChangeListener listener) {
-    this.selectedTweetChangeListener = listener;
+  public void setTwitterApi(TwitterApi twitterApi) {
+    this.twitterApi = twitterApi;
   }
 
   public void setUserIconClickedListener(TimelineAdapter.OnUserIconClickedListener listener) {
     tlAdapter.setOnUserIconClickedListener(listener);
+  }
+
+  private final UserStreamListener statusListener = new UserStreamAdapter() {
+    @Override
+    public void onStatus(final Status status) {
+      Observable.just(status)
+          .observeOn(AndroidSchedulers.mainThread())
+          .subscribe(new Action1<Status>() {
+            @Override
+            public void call(Status status) {
+              addNewStatus(status);
+            }
+          });
+    }
+
+    @Override
+    public void onDeletionNotice(final StatusDeletionNotice statusDeletionNotice) {
+      Log.d(TAG, statusDeletionNotice.toString());
+      Observable.just(statusDeletionNotice.getStatusId())
+          .observeOn(AndroidSchedulers.mainThread())
+          .subscribe(new Action1<Long>() {
+            @Override
+            public void call(Long deletedStatusId) {
+              deleteStatus(deletedStatusId);
+            }
+          });
+    }
+
+    @Override
+    public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+      Log.d(TAG, "onTrackLimitationNotice: " + numberOfLimitedStatuses);
+    }
+
+    @Override
+    public void onScrubGeo(long userId, long upToStatusId) {
+      Log.d(TAG, "onScrubGeo: " + userId + ", " + upToStatusId);
+    }
+
+    @Override
+    public void onStallWarning(StallWarning warning) {
+      Log.d(TAG, "onStallWarning: " + warning.toString());
+    }
+
+    @Override
+    public void onException(Exception ex) {
+      Log.d(TAG, "onException: " + ex.toString());
+    }
+  };
+
+  private void fetchRetweet(final long tweetId) {
+    twitterApi.retweetStatus(tweetId)
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(new Subscriber<Status>() {
+          @Override
+          public void onNext(Status status) {
+          }
+
+          @Override
+          public void onCompleted() {
+            showToast("success to retweet");
+          }
+
+          @Override
+          public void onError(Throwable e) {
+            showToast("failed to retweet...");
+          }
+        });
+  }
+
+  private void fetchFavorite(final long tweetId) {
+    twitterApi.createFavorite(tweetId)
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(new Subscriber<Status>() {
+          @Override
+          public void onNext(Status user) {
+          }
+
+          @Override
+          public void onCompleted() {
+            showToast("success to create fav.");
+          }
+
+          @Override
+          public void onError(Throwable e) {
+            Log.e(TAG, "error: ", e);
+            showToast("failed to create fav...");
+          }
+        });
+  }
+
+  private void fetchTweet() {
+    twitterApi.getHomeTimeline()
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(new Subscriber<List<Status>>() {
+          @Override
+          public void onNext(List<Status> status) {
+            addNewStatuses(status);
+          }
+
+          @Override
+          public void onCompleted() {
+          }
+
+          @Override
+          public void onError(Throwable e) {
+            Log.e(TAG, "home timeline is not downloaded.", e);
+          }
+        });
+  }
+
+  private void fetchTweet(Paging paging) {
+    twitterApi.getHomeTimeline(paging)
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(new Subscriber<List<Status>>() {
+          @Override
+          public void onNext(List<Status> status) {
+            addStatusesAtLast(status);
+          }
+
+          @Override
+          public void onCompleted() {
+          }
+
+          @Override
+          public void onError(Throwable e) {
+            Log.e(TAG, "home timeline is not downloaded.", e);
+          }
+        });
+  }
+
+  private void showToast(String text) {
+    Toast.makeText(getContext(), text, Toast.LENGTH_SHORT).show();
   }
 }
