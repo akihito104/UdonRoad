25f6b4b56334276455e268044c9bfe75ef1e26bb
matsuda akihito <matsuda104@gmail.com>
2016-07-31 13:39:13 +0900

    fixed thumbnail margin

--- app/src/main/java/com/freshdigitable/udonroad/MediaContainer.java
+++ app/src/main/java/com/freshdigitable/udonroad/MediaContainer.java
@@ -1,5 +1,17 @@
 /*
- * Copyright (c) 2016. UdonRoad by Akihito Matsuda (akihito104)
+ * Copyright (c) 2016. Akihito Matsuda (akihito104)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.freshdigitable.udonroad;
@@ -7,7 +19,6 @@ package com.freshdigitable.udonroad;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -68,8 +79,6 @@ public class MediaContainer extends LinearLayout {
     }
   }
 
-  private final LayoutParams lp = new LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT, 1);
-
   private void setThumbCount(int count) {
     this.thumbCount = count;
     final int size = getChildCount();
@@ -78,10 +87,11 @@ public class MediaContainer extends LinearLayout {
     }
     for (int i = 0; i < count - size; i++) {
       final MediaImageView mediaImageView = new MediaImageView(getContext());
-      if (size + i > 0) {
+      final LayoutParams lp = new LayoutParams(0, LayoutParams.MATCH_PARENT, 1);
+      if (size + i >= 1) {
         lp.leftMargin = grid;
       }
-      addView(mediaImageView, lp);
+      addView(mediaImageView, -1, lp);
     }
   }
 
